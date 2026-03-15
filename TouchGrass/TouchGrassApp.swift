65cd466f9c9bd8f8b34866ba49effc5a61a2ff02
Simon Fossett <simoncfossett@yahoo.com>
2026-03-15 13:55:48 -0400

    Initial Commit

---
+++ TouchGrass/TouchGrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchGrassApp.swift
+//  TouchGrass
+//
+//  Created by Simon Fossett on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct TouchGrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
